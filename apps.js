b4fffe429dcd9b3a2fbf62b86e038a4d0f5c8ac8
Richard Harvey <germoisawesome@gmail.com>
2021-06-30 23:33:45 -0700

    added more js

---
+++ apps.js
@@ -0,0 +1,3 @@
+let user_name = prompt('Please Enter Your Name ');
+console.log(user_name)
+document.write('Welcome ' + user_name + '!')
---
+++ apps2.js
@@ -0,0 +1,5 @@
+let likeGames = prompt('Do you like games?')
+if(likeGame == 'yes'){
+    document.write('I like games as well!')
+} else {
+    document.write("i am sorry to hear that.  Go back to book place thingy!")
