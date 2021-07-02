5f2de415e9c34cd200f752e2d09eabcda613f9fd
Richard Harvey <germoisawesome@gmail.com>
2021-07-02 08:30:23 -0700

    working

--- apps.js
+++ apps.js
@@ -4,7 +4,7 @@ function getTime() {
   let greeting;
   if (hourNow > 18) {
     greeting = "Good evening";
-  } else if (hourNow > 11) {
+  } else if (hourNow > 12) {
     greeting = "Good afternoon";
   } else if (hourNow >= 0) {
     greeting = "Good morning";
